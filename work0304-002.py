32ced26539b0e42c72e1d14906b7b8e3c28477bf
Shimei-Py <80047622+Shimei-Py@users.noreply.github.com>
2021-03-07 15:32:16 +0800

    Add files via upload

---
+++ work0304-002.py
@@ -0,0 +1,17 @@
+# for i in range(2,10,+2):
+#     print(i)
+# print("程式執行完畢")
+
+# total=0
+# num=int(input("請輸入數字:"))
+# for a in range(1,num+1):
+#     total += a
+# print("總和:",total)
+
+
+
+total=0
+num=int(input("請輸入1~50數字:"))
+for a in range(1,num+2):
+    total += a
+print("總和:",total)
