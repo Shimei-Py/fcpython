333cd24b7e68b088cd4f8f289c975c2ce04d0a06
Shimei-Py <80047622+Shimei-Py@users.noreply.github.com>
2021-03-07 15:49:58 +0800

    Add files via upload

---
+++ work0304-001.py
@@ -0,0 +1,12 @@
+total=0
+for a in range(50,1,-2):
+    print(a)
+    total += a
+print("偶數總和:",total)
+
+total=0
+for a in range(1,50,2):
+    print(a)
+    total += a
+print("奇數總和:",total)
+      
